58215e8c41c5d38571ced3bc3740f5362b2e98f7
bmccoy04 <bryanmccoy04@gmail.com>
2019-02-11 18:38:28 -0600

    setting up the program

---
+++ MemoryLocations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MemoryLocations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Memory location example!");
+            var run = true;
+            while(run){
+                Console.Write("Enter an option: ");
+                var option = Console.ReadLine();
+                
+                Console.WriteLine(" ");
+
+                if(option == "0" || option == string.Empty || option.ToLower() == "exit")
+                    run = false;
+                else
+                    RunOptions(option);
+                Console.WriteLine(" ");
+            }
+        }
+
+
+        public static void RunOptions(string option)
+        {
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Value Reference method");
+                    break;
+                case "2":
+                    Console.WriteLine("Value Reference w/ parameters.");
+                    break;
+                default:
+                    Console.WriteLine("Try again!");
+                    break;
+            }
+        }
+    }
+}
