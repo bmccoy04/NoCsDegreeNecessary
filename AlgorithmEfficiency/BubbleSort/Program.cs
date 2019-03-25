5d734c05114e2fa2a95e60453410f17058853e29
bmccoy04 <bryanmccoy04@gmail.com>
2019-03-24 19:44:37 -0500

    selection sort added.

--- AlgorithmEfficiency/BubbleSort/Program.cs
+++ AlgorithmEfficiency/BubbleSort/Program.cs
@@ -4,9 +4,31 @@ namespace BubbleSort
 {
     class Program
     {
+        public static int[] unOrderedNumbers = new int[] {2, 4, 1, 10, 5, 3, 7, 9, 8, 6};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(unOrderedNumbers.ToString());
+            var sortedArray = SortArray(unOrderedNumbers);
+
+            Console.WriteLine(sortedArray);
+        }
+
+        private static int[] SortArray(int[] unOrderedNumbers)
+        {
+            var sortedArray = new int[10];
+
+            for(int i = 0; i < 10; i++) {
+                var currentNumber = unOrderedNumbers[i];
+
+                for(int y = i + 1; y < 10; y++) {
+                    if(currentNumber < unOrderedNumbers[y])
+                        currentNumber = unOrderedNumbers[y];
+                }
+
+                sortedArray[i] = currentNumber;
+            }
+
+            return sortedArray;
         }
     }
 }
---
+++ AlgorithmEfficiency/SelectionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SelectionSort 
+{
+    class Program
+    {
+        public static int[] unOrderedNumbers = new int[] {2, 4, 1, 10, 5, 3, 7, 9, 8, 6};
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", unOrderedNumbers));
+            var sortedArray = SortArray(unOrderedNumbers);
+
+            Console.WriteLine(string.Join(", ", sortedArray));
+        }
+
+        private static int[] SortArray(int[] unOrderedNumbers)
+        {
+            var newArray = unOrderedNumbers;
+
+            for(int i = 0; i < 9; i++) {
+                var currentNumber = newArray[i];
+                var minIndex = i;
+
+                for(int y = i + 1; y < 10; y++) {
+
+                    var numberToBeChecked = newArray[y];
+                    var newMinNumber = newArray[minIndex];
+
+                    if(numberToBeChecked < newMinNumber)
+                        minIndex = y;
+                }
+
+                int tempNumber = newArray[minIndex];
+                newArray[minIndex] = newArray[i];
+                newArray[i] = tempNumber;
+            }
+
+            return newArray;
+        }
+    }
+}
