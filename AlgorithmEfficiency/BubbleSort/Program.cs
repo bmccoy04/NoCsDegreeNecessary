dd5dce7b34d8c4d4d333fd8205024cd4ae400fad
bmccoy04 <bryanmccoy04@gmail.com>
2019-03-24 20:24:18 -0500

    added bubble sort

--- AlgorithmEfficiency/BubbleSort/Program.cs
+++ AlgorithmEfficiency/BubbleSort/Program.cs
@@ -7,28 +7,33 @@ namespace BubbleSort
         public static int[] unOrderedNumbers = new int[] {2, 4, 1, 10, 5, 3, 7, 9, 8, 6};
         static void Main(string[] args)
         {
-            Console.WriteLine(unOrderedNumbers.ToString());
+            Console.WriteLine(string.Join(", ", unOrderedNumbers));
+
             var sortedArray = SortArray(unOrderedNumbers);
 
-            Console.WriteLine(sortedArray);
+            Console.WriteLine(string.Join(", ", sortedArray)); 
         }
 
         private static int[] SortArray(int[] unOrderedNumbers)
         {
-            var sortedArray = new int[10];
+            var newArray = unOrderedNumbers;
+            int temp;
 
-            for(int i = 0; i < 10; i++) {
-                var currentNumber = unOrderedNumbers[i];
+            for(int i = 0; i < newArray.Length - 1; i++) {
+                for(int j = 0; j < newArray.Length - 1; j++) {
+                    
+                    var current = newArray[j];
+                    var next = newArray[j + 1];
 
-                for(int y = i + 1; y < 10; y++) {
-                    if(currentNumber < unOrderedNumbers[y])
-                        currentNumber = unOrderedNumbers[y];
+                    if(current > next) {
+                        temp = next;
+                        newArray[j + 1] = current;
+                        newArray[j] = temp;
+                    }
                 }
-
-                sortedArray[i] = currentNumber;
             }
 
-            return sortedArray;
+            return newArray;
         }
     }
 }
--- AlgorithmEfficiency/MergeSort/Program.cs
+++ AlgorithmEfficiency/MergeSort/Program.cs
@@ -4,9 +4,22 @@ namespace MergeSort
 {
     class Program
     {
+        public static int[] unOrderedNumbers = new int[] {2, 4, 1, 10, 5, 3, 7, 9, 8, 6};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(string.Join(", ", unOrderedNumbers));
+
+            var sortedArray = SortArray(unOrderedNumbers);
+
+            Console.WriteLine(string.Join(", ", sortedArray)); 
+        }
+
+        private static int[] SortArray(int[] unOrderedNumbers)
+        {
+            var newArray = unOrderedNumbers;
+            int temp;
+            
+            return newArray;
         }
     }
 }
