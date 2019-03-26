c1c56f9c8fce56151298819bb06ecaba1ec5a8d5
bmccoy04 <bryanmccoy04@gmail.com>
2019-03-25 20:41:26 -0500

    added merge sort

--- AlgorithmEfficiency/MergeSort/Program.cs
+++ AlgorithmEfficiency/MergeSort/Program.cs
@@ -8,18 +8,61 @@ namespace MergeSort
         static void Main(string[] args)
         {
             Console.WriteLine(string.Join(", ", unOrderedNumbers));
+            var sortedArray = unOrderedNumbers;
 
-            var sortedArray = SortArray(unOrderedNumbers);
+            SortArray(sortedArray, 0, 9);
 
             Console.WriteLine(string.Join(", ", sortedArray)); 
         }
 
-        private static int[] SortArray(int[] unOrderedNumbers)
+        private static void SortArray(int[] sortedArray, int leftIndex, int rightIndex)
         {
-            var newArray = unOrderedNumbers;
-            int temp;
-            
-            return newArray;
+            int mid;
+
+            if(rightIndex > leftIndex)
+            {
+                mid = (rightIndex + leftIndex) / 2;
+                SortArray(sortedArray, leftIndex, mid);
+                SortArray(sortedArray, mid + 1, rightIndex);
+
+                SortMerge(sortedArray, leftIndex, mid + 1, rightIndex);
+            }
+        }
+
+        private static void SortMerge(int[] sortedArray, int leftIndex, int mid, int rightIndex)
+        {
+            int[] temp = new int[sortedArray.Length];
+            int endOfLeft = (mid - 1);
+            int number = (rightIndex - leftIndex + 1);
+            int position = leftIndex;
+
+
+            while(leftIndex <= endOfLeft && (mid <= rightIndex)) 
+            {
+                if(sortedArray[leftIndex] <= sortedArray[mid]) {
+                    temp[position++] = sortedArray[leftIndex++];
+                } else {
+                    temp[position++] = sortedArray[mid++];
+                }
+            }
+
+            while(leftIndex <= endOfLeft) 
+            {
+                temp[position++] = sortedArray[leftIndex++];
+            }
+
+            while(mid <= rightIndex)
+            {
+                temp[position++] = sortedArray[mid++];
+            }
+
+            for(int i = 0; i < number; i++)
+            {
+                sortedArray[rightIndex] = temp[rightIndex];
+
+                rightIndex--;
+            }
+
         }
     }
 }
