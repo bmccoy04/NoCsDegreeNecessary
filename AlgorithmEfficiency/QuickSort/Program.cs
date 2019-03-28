3ab32d04618ea66a059b9c81e4f353beaef4c4d8
bmccoy04 <bryanmccoy04@gmail.com>
2019-03-27 20:09:36 -0500

    quick sort

--- AlgorithmEfficiency/QuickSort/Program.cs
+++ AlgorithmEfficiency/QuickSort/Program.cs
@@ -4,9 +4,51 @@ namespace QuickSort
 {
     class Program
     {
+        public static int[] unOrderedNumbers = new int[] {2, 4, 1, 10, 5, 3, 7, 9, 8, 6};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(string.Join(", ", unOrderedNumbers));
+            var sortedArray = unOrderedNumbers;
+
+            SortArray(sortedArray, 0, 9);
+
+            Console.WriteLine(string.Join(", ", sortedArray)); 
+        }
+
+        private static void SortArray(int[] array, int low, int high)
+        {
+            if(low < high) 
+            {
+                int p = Partition(array, low, high);
+
+                SortArray(array, low, p - 1);
+                SortArray(array, p + 1, high);
+            }
+        }
+
+        private static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+
+            int index = (low - 1);
+
+            for(int j = low; j < high; j++)
+            {
+                if(array[j] <= pivot) 
+                {
+                    index++;
+
+                    var temp = array[index];
+                    array[index] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+            int temp1 = array[index + 1];
+            array[index + 1] = array[high];
+            array[high] = temp1;
+
+            return index + 1;
         }
     }
 }
